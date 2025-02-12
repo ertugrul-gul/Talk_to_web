296aa4bca1ecf3b53b76bc610d903744d5b3dd4a
ertugul0848 <114884594+ertugul0848@users.noreply.github.com>
2025-02-12 23:51:56 +0300

    few

--- Odev.py
+++ Odev.py
@@ -8,3 +8,6 @@ sayi2 = float(input('işlem yapmak için İkinci Sayıyı Giriniz: '))
 topla = sayi1 + sayi2
 
 print('Toplam:', topla)
+print("{0} {1} {2}".format("Toplam", "=", topla))
+print("{0} {1} {2}".format("Toplam", "=", sayi1 + sayi2))
+print(f"""{sayi1} + {sayi2} = {topla}""")
