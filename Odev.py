5410cf112a13ca5f30e0142728777369a262c29c
ertugul0848 <114884594+ertugul0848@users.noreply.github.com>
2025-02-12 23:44:02 +0300

    few

---
+++ Odev.py
@@ -0,0 +1,10 @@
+#ilk sayı girme
+sayi1 = float(input('işlem yapmak istediğiniz İlk Sayıyı Giriniz: '))
+
+#ikinci sayı girme
+sayi2 = float(input('işlem yapmak için İkinci Sayıyı Giriniz: '))
+
+#sayıları topla
+topla = sayi1 + sayi2
+
+print('Toplam:', topla)
