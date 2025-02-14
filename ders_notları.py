b491d1016e511aec3de45c9055b9a7b50f5e7067
ertugul0848 <114884594+ertugul0848@users.noreply.github.com>
2025-02-14 18:07:30 +0300

    few

---
+++ ders_notları.py
@@ -0,0 +1,12 @@
+"""
+Von Neuman
+What is a computer ?
+Input + Output + Memory + CPU (CU (kontrol ünitesi) + ALU (aritmetik mantık birimi))
+
+Standard Input  = keyboard
+Standard Output = terminal
+
+"""
+
+
+
