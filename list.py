92ca06098e0b7d475e1f6c6aaa2944e1a00f14c2
ertugul0848 <114884594+ertugul0848@users.noreply.github.com>
2025-03-15 22:20:27 +0300

    hw

---
+++ list.py
@@ -0,0 +1,15 @@
+""""
+listeler "BİRDEN FAZLA ÖĞREYİ TEK DEĞİŞKEN OLARAK SAKLAMAK İÇİN KULLANILIR.
+
+[.........] Şeklinde köşeli parantez ile yazılır.
+1- Sıralanır
+2- Değiştirilebilir
+3- Yenilenebilir değerlere izin verilir.
+
+"""
+
+
+my_list = ["Ertuğrul", "Aysun", "Deniz Aras"]
+print(my_list)
+print(len(my_list))
+print(my_list[0])
