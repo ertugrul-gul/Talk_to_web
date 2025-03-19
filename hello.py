69dfddaff51d4763503100aabf8c369a97cd4df8
ertugul0848 <114884594+ertugul0848@users.noreply.github.com>
2025-03-19 16:37:58 +0300

    few

--- hello.py
+++ hello.py
@@ -1,9 +1,32 @@
-# print("Hello world")
-# print("Hello ErTuĞRuL")
+import math
+import os
+import random
+import re
+import sys
+
+n = int(input("Bir sayi girinizi: "))
+
+def give_integer(n):
+    if n % 2 ==1:
+        print ("Weird")
+    elif 2<= n <= 5 and n % 2 == 0:
+        print ("Not Weird")
+    elif 6 <= n <=20 and n %2 == 0:
+        print("Weird")
+    elif n > 20 and n % 2 ==0:
+        print ("Not Weird")
+
+give_integer(n)
 
 
-# d= [{0},{1},{2}]
-# for x in d:
-#     print(x)
 
 
+if name == 'main':
+n = int(input().strip())
+    if n%2==1:
+        print("Weird")
+    else: n>=2 and n<=5:
+        print("Not Weird")
+    elif n>=6 and n<=20:
+        print("Weird")
+    else: print("Not Weird")
---
+++ my_datetime.py
@@ -0,0 +1,6 @@
+from datetime import datetime, UTC
+
+x = datetime.now()
+print(x)
+print(type(x))
+print(x.now(UTC))
